f0050020400da08e9780c7b2243ab55c5aa0035f
AngelUNC <nava72596@gmail.com>
2025-01-20 01:40:54 -0600

    Integrated

--- UserInput.py
+++ UserInput.py
@@ -1,1 +1,11 @@
+#Entrada de datos del Usuario
+print("Como se llama?", end=" ")
+nombre = input()
+print(f"Muy bien {nombre} digite un numero: ", end=" ")
+num1 = int(input())
+print(f"Digite un segundo numero {nombre}: ", end=" ")
+num2 = int(input())
+res = (num1+num2)
+print(f"El resultado es {res}")
 
+#20-01-2025
