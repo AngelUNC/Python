d5d63956e12077832df75e0c59269b7ffc7295a5
AngelUNC <nava72596@gmail.com>
2025-01-21 22:37:36 -0600

    Integrated

--- VoiceRecorder.py
+++ VoiceRecorder.py
@@ -1,1 +1,9 @@
-
+import sounddevice
+from scipy.io.wavfile import write
+fs=44100 
+second=int(input("Ingrese la Duracion de la Grabacion en Segundos: "))
+print("Grabando....\n")
+record_voice=sounddevice.rec(int(second * fs),samplerate=fs,channels=2)
+sounddevice.wait()
+write("GrabacionEjemplo.wav",fs,record_voice)
+print("Finalizando...\nPor Favor Espere...")
