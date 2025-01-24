282efe5d4a4a6b965d3c91e496e7c671697e565f
AngelUNC <nava72596@gmail.com>
2025-01-24 00:52:07 -0600

    modified:   PasswordProtectPDF.py

--- PasswordProtectPDF.py
+++ PasswordProtectPDF.py
@@ -1,9 +1,17 @@
+#24-Enero-2025 Angel Nava 12:51 AM
+#Contraseña: pdfpassword
+import getpass
 from pathlib import Path
-from PyPDF2 import PdfReader
-path = Path(__file__).parent/ "./PasswordProtectPDF/PDFExample.pdf"
+from PyPDF2 import PdfReader, PdfWriter
+path = Path(__file__).parent / "./PasswordProtectPDF/PDFExample.pdf"
+writer = PdfWriter()
 with path.open("rb") as f:
-    Pdf1 = ((f))
-    pdf_reader = PdfReader(Pdf1)
-    number_of_pages = len(pdf_reader.pages)
-
-print(f"Numero de Paginas: {number_of_pages}")
+    pdf_reader = PdfReader(f)
+    for page in pdf_reader.pages:
+        writer.add_page(page)
+password = getpass.getpass(f"Establece una Contraseña: ")
+writer.encrypt(password)
+output_path = Path(__file__).parent / "./PasswordProtectPDF/PDFEncriptado.pdf"
+with output_path.open("wb") as output_pdf:
+    writer.write(output_pdf)
+print(f"Archivo Encriptado Existosamente: {output_path}")
