fe4a9c5967dcc95600d7c541e635bc8c35a18338
AngelUNC <nava72596@gmail.com>
2025-01-24 00:23:48 -0600

    modified:   PasswordProtectPDF.py

--- PasswordProtectPDF.py
+++ PasswordProtectPDF.py
@@ -1,2 +1,9 @@
+from pathlib import Path
 from PyPDF2 import PdfReader
-PdfDesencriptado = PdfReader("")
+path = Path(__file__).parent/ "./PasswordProtectPDF/PDFExample.pdf"
+with path.open("rb") as f:
+    Pdf1 = ((f))
+    pdf_reader = PdfReader(Pdf1)
+    number_of_pages = len(pdf_reader.pages)
+
+print(f"Numero de Paginas: {number_of_pages}")
